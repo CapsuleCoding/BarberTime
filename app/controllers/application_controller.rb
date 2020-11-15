73420389e63e51f6570dbbceabc74210f5ace808
Capsulecoding <capsulecoding@icloud.com>
2020-11-15 11:56:46 -0800

    added

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found   
+  
+    def not_found
+      flash[:error] = "Not found"
+      redirect_to root_path  
+    end 
+
+    private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
+    end
+end
+    
+
